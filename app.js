497522c869bdc6ba916142ff647aaac1cb505ecb
shubha <shubhampatidar961@gmial.com>
2023-01-19 16:58:36 +0530

    secound commit

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ function check() {
 }
 const btn1 = document.querySelector(".btn-1");
 btn1.addEventListener("click", check);
+
+// this is function to check the number is prime or not //
+
+let thired = document.querySelector(".T-3");
+function prime_or_not() {
+  const number = Number(prompt());
+  if (number <= 2) {
+    return alert("provide number which is grater than two ");
+  }
+  for (let i = 2; i < number; i++) {
+    let prime = number % i;
+    if (prime === 0) {
+      thired.innerHTML = "number is not prime";
+      break;
+    } else {
+      thired.innerHTML = "number is prime ";
+    }
+  }
+}
